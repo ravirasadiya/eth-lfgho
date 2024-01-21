a1f6d8c7f0f91b8b05e7277495582bf97c6bbc02
ravirasadiya <ravirasadiya1994@gmail.com>
2024-01-21 11:52:37 +0530

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style/index.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import { WagmiConfig, createClient, chain } from 'wagmi';
-import { ConnectKitProvider, getDefaultClient } from 'connectkit';
-
-const client = createClient(
-  getDefaultClient({
-    appName: 'My App Name',
-    //infuraId: process.env.REACT_APP_INFURA_ID,
-    //alchemyId:  process.env.REACT_APP_ALCHEMY_ID,
-    chains: [chain.mainnet, chain.polygon],
-  })
-);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <WagmiConfig client={client}>
-      <ConnectKitProvider theme="auto">
-        <App />
-      </ConnectKitProvider>
-    </WagmiConfig>
+    <App />
   </React.StrictMode>
 );
 
